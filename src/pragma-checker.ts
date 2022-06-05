06623fb37e1800c625dff14004184bb0be565e7e
markw65 <mark_st@myosotissp.com>
2022-06-05 10:14:21 -0700

    More tweaks to pragma-checker

--- src/pragma-checker.ts
+++ src/pragma-checker.ts
@@ -6,50 +6,62 @@ export function pragmaChecker(ast: mctree.Program) {
   if (!comments) return;
   let index = -1;
   let comment: mctree.Comment;
+  let matchers: { quote: string; needle: string }[];
   const next = () => {
     while (++index < comments.length) {
       comment = comments[index];
-      if (comment.value.match(/^\s*@match\s+/)) {
-        break;
+      let match = comment.value.match(/^\s*@match\s+(.+)/);
+      if (!match) continue;
+      let str = match[1];
+      matchers = [];
+      while (
+        (match = str.match(/^([/%&#@"])(.+?(?<!\\)(?:\\{2})*)\1(\s+|$)/))
+      ) {
+        matchers.push({ quote: match[1], needle: match[2] });
+        str = str.substring(match[0].length);
+        if (!str.length) break;
       }
+      if (!str.length) break;
+      if (!matchers.length) {
+        match = str.match(/^(\S+)\s+$/);
+        if (match) {
+          matchers.push({ quote: '"', needle: match[1] });
+          break;
+        }
+      }
+
+      throw new Error(
+        `Build pragma '${comment.value}' is invalid. In ${
+          comment.loc!.source
+        }:${comment.loc!.start.line}`
+      );
     }
   };
   next();
   traverseAst(ast, (node) => {
     if (index >= comments.length) return false;
     if (node.start && node.start >= (comment.end || Infinity)) {
-      let match =
-        comment.value.match(
-          /^\s*@match\s+([/%&#@"])(.+(?<!\\)(?:\\{2})*)\1\s+$/
-        ) || comment.value.match(/^\s*@match\s+(\S+)\s+$/);
-      if (!match) {
-        throw new Error(
-          `Build pragma '${comment.value}' is invalid. In ${
-            comment.loc!.source
-          }:${comment.loc!.start.line}`
-        );
-      }
+      const { quote, needle } = matchers.shift()!;
       const haystack = formatAst(node).replace(/[\r\n]/g, " ");
       let found = false;
-      let needle = match[1];
-      if (match.length == 2) {
+      if (quote == '"') {
         found = haystack.includes(needle);
       } else {
-        if (needle == '"') {
-          found = haystack.includes((needle = match[2]));
-        } else {
-          const re = new RegExp((needle = match[2]));
-          found = re.test(haystack);
-        }
+        const re = new RegExp(needle);
+        found = re.test(haystack);
       }
+
       if (!found) {
         throw new Error(
-          `Didn't find '${needle}' in ${comment.loc!.source}:${
+          `Didn't find '${needle}' at ${comment.loc!.source}:${
             comment.loc!.start.line
           }`
         );
       }
-      next();
+      if (!matchers.length) {
+        next();
+      }
+      return false;
     }
     return null;
   });
