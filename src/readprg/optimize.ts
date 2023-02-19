6ef4b05fce2bf02aa49da9f448bae8c1ab4535ee
markw65 <mark_st@myosotissp.com>
2023-02-19 08:21:03 -0800

    Optimize shlv by constant to mulv

--- src/readprg/optimize.ts
+++ src/readprg/optimize.ts
@@ -1,15 +1,16 @@
 import { bytecodeToString, FuncEntry } from "./bytecode";
-import { Opcodes } from "./opcodes";
+import { Bytecode, Mulv, Opcodes } from "./opcodes";
 
 export function optimizeFunc(func: FuncEntry) {
   clearSelf(func);
 
-  // delete nops
+  // trivial transformations
   func.blocks.forEach((block) => {
     for (let i = block.bytecodes.length; i--; ) {
-      if (block.bytecodes[i].op === Opcodes.nop) {
+      const cur = block.bytecodes[i];
+      if (cur.op === Opcodes.nop) {
         block.bytecodes.splice(i, 1);
-        console.log(`Deleting nop in ${func.name}`);
+        console.log(`${func.name}: deleting nop`);
         if (i > 0) {
           console.log(
             ` - previous bytecode was ${bytecodeToString(
@@ -17,6 +18,34 @@ export function optimizeFunc(func: FuncEntry) {
               null
             )}`
           );
+        }
+      } else if (i && cur.op === Opcodes.shlv) {
+        const prev = block.bytecodes[i - 1];
+        if (prev.op === Opcodes.ipush || prev.op === Opcodes.lpush) {
+          const shift = BigInt(prev.arg);
+          if (!(shift & 63n) && prev.op === Opcodes.ipush) {
+            block.bytecodes.splice(i - 1, 2);
+            console.log(`${func.name}: deleting no-op shift`);
+            continue;
+          }
+          // note that 31 isn't safe if the other operand is a Long,
+          // because we end up multiplying by -2^31.
+          if (shift < (prev.op === Opcodes.lpush ? 64n : 31n)) {
+            const mul = 1n << shift;
+            if (prev.op === Opcodes.ipush) {
+              prev.arg = Number(mul) | 0;
+            } else {
+              prev.arg = BigInt.asIntN(64, mul);
+            }
+            console.log(
+              `${func.name}: converting shlv(${shift}) to mulv(${prev.arg})`
+            );
+
+            const mulv = cur as Bytecode as Mulv;
+            mulv.op = Opcodes.mulv;
+            mulv.size = 1;
+            delete mulv.arg;
+          }
         }
       }
     }
