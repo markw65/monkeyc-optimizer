f1708b53a80e89768ee651ebd5c5983ea3f82e4e
markw65 <mark_st@myosotissp.com>
2023-03-03 09:51:17 -0800

    Report backtraces when driver fails

--- test/test.js
+++ test/test.js
@@ -3,5 +3,5 @@ import { driver, error } from "../build/driver.cjs";
 driver()
   .then(() => console.log("Success"))
   .catch((e) => {
-    error("Failed:\n" + e.message);
+    error("Failed:\n" + (e.stack ? e.stack.toString() : e.toString()));
   });
