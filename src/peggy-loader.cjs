973c11355a6971b6b8866a94ef3018c4f68f36e7
markw65 <mark_st@myosotissp.com>
2023-04-07 17:50:55 -0700

    Remove peggy-loader (left over from webpack build)

--- src/peggy-loader.cjs
+++
@@ -1,9 +0,0 @@
-const peggy = require("peggy");
-
-module.exports = function peggy_loader(source) {
-  return peggy.generate(source, {
-    cache: false,
-    format: "es",
-    output: "source",
-  });
-};
